2cbc83b858118310a266b5926c013171461bf2fa
long <2724785961@qq.com>
2021-09-14 01:14:52 +0800

    first cmd

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('a-stock-tip.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from a-stock-tip!');
+		vscode.window.showInformationMessage('Hi，A股提示助手祝您发大财!');
 	});
 
 	context.subscriptions.push(disposable);
